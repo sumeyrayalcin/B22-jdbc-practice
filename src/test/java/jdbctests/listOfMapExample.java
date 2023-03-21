c35b9ff95c7a61e9d46ce5049640bfc518e4a5d9
Sumeyra <smyryalcin@gmail.com>
2023-03-21 12:07:40 +0000

    list of map example done

--- src/test/java/jdbctests/listOfMapExample.java
+++ src/test/java/jdbctests/listOfMapExample.java
@@ -2,7 +2,7 @@ package jdbctests;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,4 +52,62 @@ public class listOfMapExample {
 
     }
 
+    @Test
+    public void test2() throws SQLException {
+
+
+        Connection connection= DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        Statement statement =connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT first_name, last_name, salary, job_id FROM employees where rownum<6");
+
+        //in order to get column names we need resultsetmetadata
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+
+        //move to first row
+        resultSet.next();
+
+        //creating list for keeping all the rows maps
+        List<Map<String,Object>> queryData = new ArrayList<>();
+
+        Map<String,Object> row1 = new HashMap<>();
+
+        row1.put(rsmd.getColumnName(1),resultSet.getString(1));  //first part key, second part value
+        row1.put(rsmd.getColumnName(2),resultSet.getString(2));
+        row1.put(rsmd.getColumnName(3),resultSet.getString(3));
+        row1.put(rsmd.getColumnName(4),resultSet.getString(4));
+
+        System.out.println(row1.toString());
+
+        //move to second row
+        resultSet.next();
+
+        Map<String,Object> row2 = new HashMap<>();
+
+        row2.put(rsmd.getColumnName(1),resultSet.getString(1));
+        row2.put(rsmd.getColumnName(2),resultSet.getString(2));
+        row2.put(rsmd.getColumnName(3),resultSet.getString(3));
+        row2.put(rsmd.getColumnName(4),resultSet.getString(4));
+
+        System.out.println(row2.toString());
+
+        //adding rows one by one to my list
+        queryData.add(row1);
+        queryData.add(row2);
+
+
+
+
+        //close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+
 }
+
+/*
+when I need first 5 rows and total 4 columns from department table
+I need one map for every row, and in a row for every column I need keys for each column
+then I need list for all maps(for all rows)
+ */
